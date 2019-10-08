8dc2aef0e0f8c66645f85e4c19fa901418581641
QTDACNTT <ngocnguyenbn0697@gmail.com>
2019-10-08 15:50:38 +0700

    Nop bt7 Laravel

---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::group([
+    'namespace' => 'Backend',
+    'prefix' => 'admin'
+], function(){
+    Route::get('/dashboard','DashboardController@index')->name('backend.dashboard');
+});
+Route::group([
+    'namespace' => 'Backend',
+    'prefix' => 'admin'
+], function (){
+    // Trang dashboard - trang chủ admin
+    Route::get('/dashboard', 'DashboardController@index')->name('backend.dashboard');
+    // Quản lý sản phẩm
+    Route::group(['prefix' => 'products'], function(){
+       Route::get('/', 'ProductController@index')->name('backend.product.index');
+       Route::get('/create', 'ProductController@create')->name('backend.product.create');
+    });
+});
+
+Route::group([
+    'namespace' => 'Backend',
+    'prefix' => 'admin'
+], function (){
+    // Trang dashboard - trang chủ admin
+    Route::get('/dashboard', 'DashboardController@index')->name('backend.dashboard');
+    // Quản lý sản phẩm
+    Route::group(['prefix' => 'products'], function(){
+       Route::get('/', 'ProductController@index')->name('backend.product.index');
+       Route::get('/create', 'ProductController@create')->name('backend.product.create');
+    });
+    //Quản lý người dùng
+    Route::group(['prefix' => 'users'], function(){
+        Route::get('/', 'UserController@index')->name('backend.user.index');
+        Route::get('/create', 'UserController@create')->name('backend.user.create');
+    });
+    //Quản lý danh mục sản phẩm
+    Route::group(['prefix' => 'categories'], function(){
+        Route::get('/', 'CategoryController@index')->name('backend.category.index');
+        Route::get('/create', 'CategoryController@create')->name('backend.category.create');
+    });
+});
